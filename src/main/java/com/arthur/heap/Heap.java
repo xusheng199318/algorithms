fe365fc52655ad55bf18fd8a45822ec383336f9b
arthur <465034663@qq.com>
2019-04-27 21:21:51 +0800

    提交堆

--- src/main/java/com/arthur/heap/Heap.java
+++ src/main/java/com/arthur/heap/Heap.java
@@ -2,4 +2,67 @@ package com.arthur.heap;
 
 public class Heap {
 
+    private static int[] arr = null;
+
+    private static void initArr() {
+        arr = new int[]{53, 17, 78, 9, 45, 65, 87, 23, 8, 20};
+    }
+
+    public static void main(String[] args) {
+        initArr();
+        buildHeap();
+        for (int ele : arr) {
+            System.out.println(ele);
+        }
+    }
+
+    /**
+     * 建立最小堆，使用向上渗透方法
+     */
+    private static void buildHeap() {
+        int lastEleIndex = arr.length - 1;
+        int currentIndex = (lastEleIndex - 1) / 2;
+        while (currentIndex >= 0) {
+            shifDown(currentIndex);
+            currentIndex--;
+
+        }
+    }
+
+    public static void shifDown(int currentIndex) {
+
+        //如果是叶子节点，则不再进行比较
+        if (currentIndex > (arr.length - 2) / 2) {
+            return;
+        }
+        //获取该元素的2个子节点中的最小元素下标
+        int minChildIndex = currentIndex * 2 + 1;
+
+
+        if (validEle(minChildIndex) && arr[minChildIndex] > arr[minChildIndex + 1]) {
+            minChildIndex++;
+        }
+
+        //如果父节点比2个子节点中的最小值都小，说明不需要调整
+        if (arr[currentIndex] < arr[minChildIndex]) {
+            return;
+        }
+
+        //如果父节点大于其2个子节点中的最小元素，则与最小元素交换
+        //且需要从最小元素节点继续向下调整
+        swap(currentIndex, minChildIndex);
+        shifDown(minChildIndex);
+    }
+
+    private static boolean validEle(int eleIndex) {
+        return (eleIndex <= arr.length - 1) && (eleIndex + 1 <= arr.length - 1);
+    }
+
+
+
+    private static void swap(int fitstEleIndex, int secondEleIndex) {
+        int temp = arr[fitstEleIndex];
+        arr[fitstEleIndex] = arr[secondEleIndex];
+        arr[secondEleIndex] = temp;
+    }
 }
